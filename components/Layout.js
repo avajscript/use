f3628a8cb7b6616612d56c61d6aa46c46ef6bded
Matthew Pierce <matt-pierce@hotmail.com>
2023-01-04 13:42:18 -0500

    c

---
+++ components/Layout.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import styled from "styled-components";
+import { AnalyticsWrapper } from "./analytics";
+
+const Center = styled.div`
+  width: 100%;
+  background-color: #eee2dc;
+`;
+
+const Layout = ({ children, value }) => {
+  return (
+    <>
+      <Navbar />
+
+      <Center>
+        {children}
+        <AnalyticsWrapper />
+      </Center>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
